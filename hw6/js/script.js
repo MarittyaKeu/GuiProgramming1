ffe6bab480cacfe2891c16d7ae0531d43532344d
Marittya Keu <marittya_keu@Marittyas-MacBook-Pro.local>
2018-11-15 16:28:13 -0500

    updated css

--- hw6/js/script.js
+++ hw6/js/script.js
@@ -10,17 +10,6 @@ function doSomething() {
     var vstart = document.getElementById('vstart').value;
     var vend = document.getElementById('vend').value;
 
-    if (hend < hstart) {
-      document.getElementById("hend").style.border = "3px solid red";
-      alert("Horizontal end must be a greater value than start.")
-      return;
-    }
-
-    if (vend < vstart) {
-      document.getElementById("vend").style.border = "3px solid red";
-      alert("Vertical end must be a greater value than start.")
-      return;
-    }
 
     // highlight input border red if any input field is empty
     if (vstart == "" ) {
@@ -52,23 +41,38 @@ function doSomething() {
     }
 
 
+    /*
+    Validate to check if the the end value is greater than start value
+    */
+    if (hend < hstart) {
+      document.getElementById("hend").style.border = "3px solid red";
+      alert("Horizontal end must be a greater value than start.")
+      return;
+    }
 
-    if (vstart )
-    // build up header variable on the top
-    // to prepend to main table variable
+    if (vend < vstart) {
+      document.getElementById("vend").style.border = "3px solid red";
+      alert("Vertical end must be a greater value than start.")
+      return;
+    }
+
+    /*
+     build up header variable on the top to prepend to main table variable
+    */
     var temp = "";
-    var head = '<tr> <th></th>';
+    var head = '<tr> <th class="firstth"></th>';
     for(var k= vstart; k <= vend; k++) {
-      head += '<th>' + (k) + '</th>';
+      head += '<th class="thead">' + (k) + '</th>';
     }
     head += '</tr>';
 
-    // dynamic multiplication table
-    // first for loop is to populate row
-    // second for loop is to populate column
-    // build up a variable to create the tables, row, and column
+    /*
+    Dynamic multiplcation table
+    the first loop populates the rows and the
+    second for loop populates the columns
+    */
     for(var i = hstart; i <= hend; i++) {
-      var row = "<tr>" + '<th>' + i + '</th>' ;
+      var row = "<tr>" + '<th class="thead2">' + i + '</th>' ;
       for(var j = vstart; j <= vend; j++) {
         row +=  '<td>' + (i * j) + '</td>';
       }
@@ -87,5 +91,4 @@ function doSomething() {
   document.getElementById("hstart").style.border = null;
   document.getElementById("vstart").style.border = null;
   document.getElementById("vend").style.border = null;
-
 }
